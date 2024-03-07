7cfe5fdec1bde2be700085d06d7697eb3975e94f
jaeunP <okn2432@gmail.com>
2024-03-07 23:08:19 +0900

    test commit

---
+++ HPMP/src/main/java/com/HPMP/HpmpApplication.java
@@ -0,0 +1,13 @@
+package com.HPMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpmpApplication.class, args);
+	}
+
+}
---
+++ HPMP/src/test/java/com/HPMP/HpmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HPMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
